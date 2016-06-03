98e9a6f153b19492cb82167d170e5e11306b0e2f
Ajado Aduragbemi Joseph <agbemi19@gmail.com>
2016-06-03 21:23:13 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,36 @@
+import MergeSort;
+import java.util.*;
+
+/**
+ *
+ * @author dell
+ */
+public class Main {
+    public static void main(String[] args) {
+        Integer[] iList = {11,5,8,2,3,1,13,7,4,};
+        MergeSort<Integer> items = new MergeSort<>(iList);
+        
+        Vector<Integer> resI = items.mergeSort();
+        
+        Iterator iIt = resI.iterator();
+        while(iIt.hasNext()){
+            System.out.print(iIt.next() + ", ");
+        }
+        System.out.println();
+        
+        
+        
+        String[] sList = {"Oliver Queen","Barry Allen",
+                          "Bruce Wayne","Princess Diana",
+                          "Clark Kent", "Alan Scott",
+                          "J'onn j'onzz", "..."};
+        MergeSort sItems = new MergeSort();
+        sItems.getElems(sList);
+        
+        Vector resS = sItems.mergeSort();
+        Iterator sIt = resS.iterator();
+        while(sIt.hasNext()){
+            System.out.println(sIt.next());
+        }
+    }   
+}
